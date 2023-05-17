a913c539ec8cc1ea8caf1b8e12bd03b7acae82d0
khalil <khalilonline99@gmail.com>
2023-05-17 16:11:30 +0600

    deployment

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const productsCollection = client.db("teeStore").collection("products");
 
 // JWT Verification
-// function verifyJWT(req, res, next) {
-//   const authHeader = req.headers.authorization;
-//   if (!authHeader) {
-//     return res.status(401).send({ message: 'Unauthorized Access :(' });
-//   }
-//   const token = authHeader.split(' ')[1];
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-//     if (err) {
-//       return res.status(403).send({ message: 'Forbidden access :@' });
-//     }
-//     req.decoded = decoded;
-//     next()
-//   })
-// }
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'Unauthorized Access :(' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access :@' });
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
 
 async function run() {
 
